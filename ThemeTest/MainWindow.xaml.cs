fc2c382443511b4d6992712787cbb8cca150fc86
Tino Mclaren - mlap <m.mccarrick1@gmail.com>
2023-02-26 16:40:13 +0000

    Add project files.

---
+++ ThemeTest/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ThemeTest
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn_open_win_Click(object sender, RoutedEventArgs e)
+        {
+            Test_Win tw = new Test_Win();
+            tw.Activate();  
+        }
+
+        private void btn_open_winEx_Click(object sender, RoutedEventArgs e)
+        {
+            Test_Win_Ex tw = new Test_Win_Ex();
+            tw.Activate();
+        }
+    }
+}
---
+++ ThemeTest/Test_Win_Ex.xaml.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinUIEx;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ThemeTest
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Test_Win_Ex : WindowEx
+    {
+        public Test_Win_Ex()
+        {
+            this.InitializeComponent();
+           
+            // set theme dark
+            var content = this.Content as FrameworkElement;
+            content.RequestedTheme = ElementTheme.Dark;
+        }
+    }
+}
